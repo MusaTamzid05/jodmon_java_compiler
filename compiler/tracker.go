a678a6e3a65b38bbb37f0c1976c95be77a106386
MusaTamzid05 <tamzid101@gmail.com>
2021-12-15 00:23:26 +0600

    Main.java ignore.

--- compiler/tracker.go
+++ compiler/tracker.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,6 +14,10 @@ type Tracker struct {
 func (t *Tracker) initListFiles(path string) error {
 	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if strings.HasSuffix(path, ".java") {
+			if strings.HasSuffix(path, "Main.java") {
+				return nil
+			}
+
 			t.paths = append(t.paths, path)
 		}
 
@@ -25,6 +30,10 @@ func (t *Tracker) initListFiles(path string) error {
 }
 
 func (t *Tracker) Run() {
+
+	for _, path := range t.paths {
+		fmt.Println(path)
+	}
 }
 
 func MakeTracker(path string) (Tracker, error) {
--- main.go
+++ main.go
@@ -14,6 +14,6 @@ func main() {
 		return
 	}
 
-	tracker.Show()
+	tracker.Run()
 
 }
