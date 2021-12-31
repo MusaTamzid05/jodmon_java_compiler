a064a187947920c8e75655ceea127a27681d4bdf
MusaTamzid05 <tamzid101@gmail.com>
2022-01-01 01:15:08 +0600

    Running the program  on the current directory.

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"java_compiler_automater2/compiler"
+	"os"
 )
 
 func main() {
-	path := "/home/musa/go_workspace/src/java_compiler_automater2"
+	path, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("[*] Running on ", path)
+
 	tracker, err := compiler.MakeTracker(path)
 
 	if err != nil {
