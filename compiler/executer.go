2677ad300b537c29cece4aa36094a05404aecb31
MusaTamzid05 <tamzid101@gmail.com>
2022-01-01 02:07:29 +0600

    now showing proper error message.

--- compiler/executer.go
+++ compiler/executer.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 )
@@ -8,9 +9,15 @@ import (
 func ExecuteJavaCompile(path string) {
 	cmd := exec.Command("/usr/lib64/openjdk-8/bin/javac", path)
 
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
 	err := cmd.Run()
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(fmt.Sprint(err) + " : " + stderr.String())
 	}
 }
